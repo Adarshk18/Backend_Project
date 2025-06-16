d6de269f625da079231a9469531c839c3e9867bc
FOODOHOLIC <adarshkohli183@gmail.com>
2025-06-16 20:52:31 +0530

    new

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const express = require("express");
 const app= express();
 const userModel = require("./models/user");
 const path = require("path");
+const cookieParser = require("cookie-parser");
 
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
 
 
 app.get("/",(req,res)=>{
-    res.send("hiii");
+    res.render("index");
 });
 
 app.post("/create",(req,res)=>{
