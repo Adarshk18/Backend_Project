297c8336af0392a91bd74520261970a84201734a
FOODOHOLIC <adarshkohli183@gmail.com>
2025-06-17 10:27:32 +0530

    new

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const app= express();
+const app = express();
 const userModel = require("./models/user");
+const postModel = require("./models/post");
 const path = require("path");
 const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -10,21 +12,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
 
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.render("index");
 });
 
-app.post("/create",(req,res)=>{
-    let {username, name, age, email, password} = req.body;
-    let user = userModel.create({
-        username,
-        name,
-        age,
-        email,
-        password,
-    });
+app.post("/register", async (req, res) => {
+    let { username, name, age, email, password } = req.body;
+    let user = await userModel.findOne({ email })
+    if (user) return res.status(201).send({ message: "User already exists" })
 
-    res.send(user);
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(password, salt, async (err, hash) => {
+            let createdUser = await userModel.create({
+                username,
+                name,
+                age,
+                email,
+                password,
+            });
+            let token = jwt.sign({email: email, userid: user._id}, "sssshh");
+            res.cookie("token",token);
+            res.send("user registered");
+        })
+
+    })
+
+
+
+
 })
 
 app.listen(3000);
