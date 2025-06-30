3709c63605c58cb82e6bd83d5c12cd06d13fdb14
Adarshk18 <adarshkohli183@gmail.com>
2025-06-30 12:31:59 +0530

    new

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const crypto = require("crypto");
+const multerConfig = require("./config/multerconfig");
+const upload = require("./config/multerconfig");
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -14,9 +17,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
 
 
+
 app.get("/", (req, res) => {
     res.render("index");
 });
+
+app.get("/profile/upload", (req, res) => {
+    res.render("profileupload");
+});
+
+app.post("/upload", isLoggedIn,upload.single("image") ,async(req, res) => {
+    let user = await userModel.findOne({email: req.user.email});
+    user.profilepic = req.file.filename;
+    await user.save();
+    res.redirect("/profile");
+});
+
+
 
 app.post("/register", async (req, res) => {
     let { username, name, age, email, password } = req.body;
@@ -32,42 +49,54 @@ app.post("/register", async (req, res) => {
                 email,
                 password: hash,
             });
-            let token = jwt.sign({email: email, userid: user._id}, "sssshh");
-            res.cookie("token",token);
+            let token = jwt.sign({ email: email, userid: user._id }, "sssshh");
+            res.cookie("token", token);
             res.send("user registered");
         })
     })
 })
 
-app.get("/login",(req,res)=>{
+app.get("/login", (req, res) => {
     res.render("login")
 })
 
-app.get("/profile",isLoggedIn, async(req,res)=>{
-    let user  = await userModel.findOne({email: req.user.email}).populate("posts");
-    
-    res.render("profile", {user});
+app.get("/profile", isLoggedIn, async (req, res) => {
+    let user = await userModel.findOne({ email: req.user.email }).populate("posts");
+
+    res.render("profile", { user });
 });
 
-app.get("/like/:id",isLoggedIn, async(req,res)=>{
-    let post  = await postModel.findOne({_id: req.params.id}).populate("user");
+app.get("/like/:id", isLoggedIn, async (req, res) => {
+    let post = await postModel.findOne({ _id: req.params.id }).populate("user");
 
-    if(post.likes.indexOf(req.user.userid)==-1){
+    if (post.likes.indexOf(req.user.userid) == -1) {
         post.likes.push(req.user.userid);
-    }else{
-        post.likes.splice(post.likes.indexOf(req.user.userid),1);
+    } else {
+        post.likes.splice(post.likes.indexOf(req.user.userid), 1);
     }
 
-    
+
     await post.save();
     res.redirect("/profile");
 });
 
-app.post("/post",isLoggedIn, async(req,res)=>{
-    let user  = await userModel.findOne({email: req.user.email});
-    let {content} = req.body;
+app.get("/edit/:id", isLoggedIn, async (req, res) => {
+    let post = await postModel.findOne({ _id: req.params.id }).populate("user");
 
-    let post  = await postModel.create({
+    res.render("edit", { post });
+});
+
+app.post("/update/:id", isLoggedIn, async (req, res) => {
+    let post = await postModel.findOneAndUpdate({ _id: req.params.id }, { content: req.body.content })
+
+    res.redirect("/profile")
+});
+
+app.post("/post", isLoggedIn, async (req, res) => {
+    let user = await userModel.findOne({ email: req.user.email });
+    let { content } = req.body;
+
+    let post = await postModel.create({
         user: user._id,
         content,
     });
@@ -75,28 +104,28 @@ app.post("/post",isLoggedIn, async(req,res)=>{
     user.posts.push(post._id);
     await user.save();
     res.redirect("/profile");
-   
+
 });
 
 app.post("/login", async (req, res) => {
-    let {email, password } = req.body;
+    let { email, password } = req.body;
 
     let user = await userModel.findOne({ email })
     if (!user) return res.status(500).send("something went wrong..")
 
-    bcrypt.compare(password, user.password, (err,result)=>{
-        if(result){
-            
-            let token = jwt.sign({email: email, userid: user._id}, "sssshh");
-            res.cookie("token",token);
+    bcrypt.compare(password, user.password, (err, result) => {
+        if (result) {
+
+            let token = jwt.sign({ email: email, userid: user._id }, "sssshh");
+            res.cookie("token", token);
             res.redirect("/profile")
-            
-        } 
+
+        }
         else res.redirect("/login");
     })
 });
 
-app.get("/logout",(req,res)=>{
+app.get("/logout", (req, res) => {
     res.cookie("token", "");
     res.redirect("/login");
 })
